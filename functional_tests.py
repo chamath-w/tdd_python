b91c9c7445a24be85127e12b2f9e0028db76b3fb
Chamath Wijeratne <chamath.wijeratne@gmail.com>
2018-06-07 13:39:32 +1200

    Added Helper function to functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(
+            row_text, 
+            [row.text for row in rows],
+            f"New to-do item did not appear in table. Contents were:\n{table.text}",)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes tocheck out its homepage
         self.browser.get('http://localhost:8000')
@@ -32,12 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists "1. Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-           '1: Buy peacock feathers', [row.text for row in rows], 
-           f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        self.check_for_row_in_list_table(
+            '1: Buy peacock feathers', )
 
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,21 +50,15 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-           '1: Buy peacock feathers', [row.text for row in rows], 
-           f"New to-do item did not appear in table. Contents were:\n{table.text}")
-        self.assertIn(
-           '2: Use peacock feathers to make a fly', [row.text for row in rows], 
-           f"New to-do item did not appear in table. Contents were:\n{table.text}")
-
+        self.check_for_row_in_list_table(
+           '2: Use peacock feathers to make a fly',)
         #Edith wonders whether the site will rememeber her list. Then she sees that hte site has generated a unique URL for her -- there is some explanatory text to that effect.
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
         # Statisfied, she goes back to sleep
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
